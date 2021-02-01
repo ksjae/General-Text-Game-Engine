1f8cf9470abc2231fc54b9d861cf41f5dd5c598a
Seungjae Kim <ksjae0705@hotmail.com>
2021-02-01 11:41:07 +0900

    Initial Commit

---
+++ Shared/Text_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_GameApp.swift
+//  Shared
+//
+//  Created by 김승재 on 2021/02/01.
+//
+
+import SwiftUI
+
+@main
+struct Text_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
